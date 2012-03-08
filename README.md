51d4842613b118138ada56c6dfda748c0cbfb117
Daniel Smith <javajoint@gmail.com>
2012-03-08 11:03:28 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+
+## MultiLink - Links With Choice
+_Daniel Smith - javajoint at gmail dot com_
+_8 March 2012_
+
+MultiLink enables a menu of multiple web addresses from a click.  It's a great way to provide choice.  A picture tells the story:
+
+<img style="border: 1px solid black" src="http://daniel.org/buckybits/wp-content/uploads/2012/02/ml2-brooklyn-example.png">
+
+### Check out the Demo: http://daniel.org/demos/MultiLink3/ 
+### Blog Post: http://daniel.org/cafebucky/2012/03/01/one-link-many-paths-clicking-choice-multilink-part-3/
+
+MultiLink is built on 3 ideas:
+
+* use the HTML5 "data" attribute to refer to lists of Link IDs and/or Tags
+* use blocks of JSON data to organize IDs, Labels, URLs, and Tags
+* tie it together with JavaScript (making good use of jQuery)
+
+## Repository
+
+The Gihub repo is a simple clone of my demo.  It's meant to be self-contained, with the lone exception being an include to get jQuery from code.jquery.com
+
+## Quick Concepts
+
+On the HTML side, a data attribute is set up such as:
+
+    <a id="beer1" class="multilink"
+         data-dls-links="sierranevada, fosters, pilsnerurquell">beer</a>
+
+
+Which is a simple list that is matched up with some data that originated from a block of JSON:
+
+
+        "sierranevada": {
+            "label" : "Sierra Nevada",
+            "url"   : "http://www.sierranevada.com/",
+            "tags"  : ["beer", "ale"]
+        },
+
+It gets much more interesting when you look for tags, instead of absolute Link IDs:
+
+    <ul>
+        <li>Here is <a id="nyctag" class="multilink" data-dls-links=".nyc">"nyc"</a></li>
+        <li>and ... <a id="bridgetags" class="multilink" data-dls-links=".bridge">"bridge"</a></li>
+
+You can have lists of tags (".la, .nyc. ,sf, .london"), which will simply add whatever links are matched.
+
+You can also get very specific:
+
+        <li><a id="nycandbridge" class="multilink" data-dls-links=".nyc + .bridge">"nyc AND bridge (+)"</a></li>
+        <li><a id="nycwobridge" class="multilink" data-dls-links=".nyc - .bridge">"nyc WITHOUT bridge (-)"</a></li>
+        <li><a id="nycorbridge" class="multilink" data-dls-links=".nyc | .bridge">"nyc OR bridge (|)"</a></li>
+        <li><a id="nycorbridgenolon" class="multilink" data-dls-links=".nyc | .bridge - .london">"nyc OR bridge, without London (| and -)"</a></li>
+    </ul>
+
+There is much more detail to be found in the <a href="http://daniel.org/demos/MultiLink3/">Demo</a> and the <a href-"http://daniel.org/cafebucky/2012/03/01/one-link-many-paths-clicking-choice-multilink-part-3/">Blog Post</a>
+
+## Summary
+
+MultiLink provides a powerful, flexible, and simple to set up means of giving choices for clickable items on a page.  It's not meant to replace menus used for site navigation, but rather, to open up the ideas of making the writing style of a site easer to read (how many times have you seen someone say "additional info at here, here, or here", when they could write "additional info at these places").  It also opens up the idea of writing HTML that uses placeholders for links, with the details of how they get resolved left to the actual data present when the item is clicked.
+
+
+
+
---
+++ js/Config.js
@@ -0,0 +1,234 @@
+/*
+**	Config.js
+*/
+
+ML.Config = {
+	"allLinks" : {
+
+		"bmwe36": {
+			"label"	: "BMW (E36) - Wikipedia",
+			"url"	: "http://en.wikipedia.org/wiki/BMW_3_Series_(E36)",
+			"tags"	: ["e36m3", "bmw", "car", "germany"]
+		},
+
+
+		"vwbug": {
+			"label"	: "VW Bug",
+			"url"	: "http://en.wikipedia.org/wiki/Volkswagen_Beetle",
+			"tags"	: ["vw", "car", "germany"]
+		},
+
+
+		"sierranevada": {
+			"label"	: "Sierra Nevada",
+			"url"	: "http://www.sierranevada.com/",
+			"tags"	: ["beer", "ale"]
+		},
+
+
+		"fosters": {
+			"label"	: "Fosters Beer",
+			"url"	: "http://www.fostersbeer.com/",
+			"tags"	: ["australia", "beer", "lager"]
+		},
+
+
+		"pilsnerurquell": {
+			"label"	: "Pilsner Urquell",
+			"url"	: "http://www.pilsnerurquell.com/us-home",
+			"tags"	: ["beer", "pilsner"]
+		},
+
+
+		"radeberger": {
+			"label"	: "Radeberger Pilsner",
+			"url"	: "http://goo.gl/F7vhs",
+			"tags"	: ["beer", "pilsner", "germany"]
+		},
+
+
+		"acrecoffee": {
+			"label"	: "Acre Coffee",
+			"url"	: "http://www.acrecoffee.com/",
+			"tags"	: ["cafe", "petaluma", "coffee"]
+		},
+
+
+		"wsbistro": {
+			"label"	: "Water Street Bistro",
+			"url"	: "http://www.waterstreetbistro.net",
+			"tags"	: ["cafe", "petaluma", "frenchfood"]
+		},
+
+
+		"aqus": {
+			"label"	: "Aqus Cafe",
+			"url"	: "http://aquscafe.com/",
+			"tags"	: ["cafe", "petaluma", "beer", "community", "coffee"]
+		},
+
+
+		"jodiefoster": {
+			"label"	: "Jodie Foster",
+			"url"	: "http://en.wikipedia.org/wiki/Jodie_Foster",
+			"tags"	: ["actress"]
+		},
+
+
+		"gracehopper": {
+			"label"	: "Grace Hopper",
+			"url"	: "http://en.wikipedia.org/wiki/Grace_Hopper",
+			"tags"	: ["navy", "computerscientist", "innovator"]
+		},
+
+
+		"mmayer": {
+			"label"	: "Marissa Mayer",
+			"url"	: "http://en.wikipedia.org/wiki/Marissa_Mayer",
+			"tags"	: ["computerscientist", "google"]
+		},
+
+
+		"nychighline": {
+			"label"	: "The High Line",
+			"url"	: "http://www.thehighline.org/",
+			"tags"	: ["nyc", "park", "manhattan", "city"]
+		},
+
+
+		"centralpark": {
+			"label"	: "The Official Website of Central Park",
+			"url"	: "http://www.centralparknyc.org",
+			"tags"	: ["nyc", "park", "manhattan", "city"]
+		},
+
+
+		"prospectpark": {
+			"label"	: "Prospect Park",
+			"url"	: "http://www.prospectpark.org/",
+			"tags"	: ["nyc", "park", "brooklyn"]
+		},
+
+		"mtainfo": {
+			"label"	: "mta.info - transit",
+			"url"	: "http://www.mta.info",
+			"tags"	: ["nyc", "transit", "subway", "bus"]
+		},
+
+
+		"brooklynbridge": {
+			"label"	: "Brooklyn Bridge",
+			"url"	: "http://en.wikipedia.org/wiki/Brooklyn_Bridge",
+			"tags"	: ["nyc", "bridge", "brooklyn", "manhattan"]
+		},
+
+		"manhattanbridge": {
+			"label"	: "Manhattan Bridge",
+			"url"	: "http://en.wikipedia.org/wiki/Manhattan_Bridge",
+			"tags"	: ["nyc", "bridge", "manhattan"]
+		},
+
+
+		"visitbrooklyn": {
+			"label"	: "Visit Brooklyn",
+			"url"	: "http://www.visitbrooklyn.org/",
+			"tags"	: ["nyc", "brooklyn", "city"]
+		},
+
+
+		"brooklynbrewery": {
+			"label"	: "Brooklyn Brewery",
+			"url"	: "http://brooklynbrewery.com/",
+			"tags"	: ["nyc", "brooklyn", "beer", "pub", "cafe"]
+		},
+
+
+		"sydneyoz": {
+			"label"	: "Sydney, Australia",
+			"url"	: "http://en.wikipedia.org/wiki/Sydney",
+			"tags"	: ["australia", "sydney", "city"]
+		},
+
+
+		"sydneybridgeclimb": {
+			"label"	: "Sydney Harbour Bridge Tour",
+			"url"	: "http://www.bridgeclimb.com",
+			"tags"	: ["australia", "sydney", "tour"]
+		},
+
+
+		"sydneybotanical": {
+			"label"	: "Sydney Botanical Gardens",
+			"url"	: "http://www.rbgsyd.nsw.gov.au/",
+			"tags"	: ["australia", "sydney", "park"]
+		},
+
+
+		"bluemountainsoz": {
+			"label"	: "Blue Mountains",
+			"url"	: "http://www.bluemts.com.au/",
+			"tags"	: ["australia", "mountain", "park"]
+		},
+
+		"barossaoz": {
+			"label"	: "Australia - Barossa Valley Wine Region",
+			"url"	: "http://www.barossa.com/",
+			"tags"	: ["australia", "wine"]
+		},
+
+		"melbournepz": {
+			"label"	: "Melbourne",
+			"url"	: "http://www.visitmelbourne.com/",
+			"tags"	: ["australia", "melbourne", "city"]
+		},
+
+		"melbourneapoz": {
+			"label"	: "Melbourne - Albert Park / F1",
+			"url"	: "http://en.wikipedia.org/wiki/Melbourne_Grand_Prix_Circuit",
+			"tags"	: ["australia", "melbourne", "city", "park"]
+		},
+
+		"ggbridge": {
+			"label"	: "Golden Gate Bridge",
+			"url"	: "http://en.wikipedia.org/wiki/Golden_Gate_Bridge",
+			"tags"	: ["sanfrancisco", "bridge"]
+		},
+
+		"parispontneuf": {
+			"label"	: "Paris - Pont Neuf",
+			"url"	: "http://en.wikipedia.org/wiki/Pont_Neuf",
+			"tags"	: ["paris", "city", "bridge"]
+		},
+
+		"londontowerbridge": {
+			"label"	: "London - Tower Bridge",
+			"url"	: "http://en.wikipedia.org/wiki/Tower_Bridge",
+			"tags"	: ["london", "city", "bridge"]
+		},
+
+		"londonhyde": {
+			"label"	: "London - Hyde Park",
+			"url"	: "http://www.royalparks.gov.uk/Hyde-Park.aspx",
+			"tags"	: ["london", "city", "park"]
+		},
+
+		"mlblog1": {
+			"label"	: "One Link, Many Paths, Clicking Choice! (MultiLink, Part 1)",
+			"url"	: "http://daniel.org/cafebucky/2012/02/26/one-link-many-paths-clicking-choice-multilink-part-1/",
+			"tags"	: ["multilink", "blog"]
+		},
+
+		"mlblog2": {
+			"label"	: "(MultiLink, Part 2) - where I introduce tags...",
+			"url"	: "http://daniel.org/cafebucky/2012/02/27/one-link-many-paths-clicking-choice-multilink-part-2/",
+			"tags"	: ["multilink", "blog"]
+		},
+
+		"mlblog3": {
+			"label"	: "(MultiLink, Part 3) - ID Includes and Tag Expressions...",
+			"url"	: "http://daniel.org/cafebucky/2012/03/01/one-link-many-paths-clicking-choice-multilink-part-3/",
+			"tags"	: ["multilink", "blog"]
+		}
+
+	}
+}
